cdd91a9bf76bef56a6ed3ef1e092297b2bbdda4a
12kkot <kotk6133@gmail.com>
2025-04-04 21:17:40 +0700

    Add files via upload

---
+++ products.php
@@ -0,0 +1,116 @@
+<?php
+session_start();
+
+?>
+<!DOCTYPE html>
+<html lang="en" style="--gradient-color: linear-gradient(330deg,rgb(41, 60, 234) 0%,rgb(8, 105, 251) 100%); --gradient-hover-color: linear-gradient(330deg,rgb(48, 37, 207) 0%,rgb(9, 74, 214) 100%); --gradient-focus-color: linear-gradient(330deg,rgb(34, 83, rgb(9, 75, 197) #4809c5 100%); --mouse-x: 1909px; --mouse-y: 1px;" <head=""><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="keywords" content="Expensive, Expensive nextgen, nurik, wexside, celestial, akrien, minecraft, minecraft 1.8, minecraft 1.12.2, minecraft 1.16.5, deadcode, нурсултан, Wonderful client, нурсултан клиент, нурик, вексайд, целестиал, акриен, майнкрафт, чит, читы, читы для майнкрафт, читы для minecraft, чит на майнкрафт, чит на minecraft">
+    <meta name="description" content="Expensive client - Лучший чит-клиент для комфортной игры. Для майнкрафт 1.16.5">
+    <title id="title">Expensive - Лучший клиент для комфортной игры.</title>
+    <link href="all3/all.min.css" rel="stylesheet" crossorigin="use-credentials">
+    <link href="all3/animate.min.css" rel="stylesheet">
+    <script defer="defer" src="all3/main.ac9aa76c.js.Без названия"></script>
+    <link href="all3/main.652df174.css" rel="stylesheet">
+    <link href="css/border.css" rel="stylesheet">
+            <script src="all3/767b1751eb.js" crossorigin="anonymous"></script><style media="all" id="fa-v4-font-face">/*!
+ * Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com
+ * License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
+ * Copyright 2024 Fonticons, Inc.
+ */@font-face{font-family:"FontAwesome";font-display:block;src:url(https://ka-f.fontawesome.com/releases/v6.5.2/webfonts/free-fa-solid-900.woff2) format("woff2"),url(https://ka-f.fontawesome.com/releases/v6.5.2/webfonts/free-fa-solid-900.ttf) format("truetype")}@font-face{font-family:"FontAwesome";font-display:block;src:url(https://ka-f.fontawesome.com/releases/v6.5.2/webfonts/free-fa-brands-400.woff2) format("woff2"),url(https://ka-f.fontawesome.com/releases/v6.5.2/webfonts/free-fa-brands-400.ttf) format("truetype")}@font-face{font-family:"FontAwesome";font-display:block;src:url(https://ka-f.fontawesome.com/releases/v6.5.2/webfonts/free-fa-regular-400.woff2) format("woff2"),url(https://ka-f.fontawesome.com/releases/v6.5.2/webfonts/free-fa-regular-400.ttf) format("truetype");unicode-range:u+f003,u+f006,u+f014,u+f016-f017,u+f01a-f01b,u+f01d,u+f022,u+f03e,u+f044,u+f046,u+f05c-f05d,u+f06e,u+f070,u+f087-f088,u+f08a,u+f094,u+f096-f097,u+f09d,u+f0a0,u+f0a2,u+f0a4-f0a7,u+f0c5,u+f0c7,u+f0e5-f0e6,u+f0eb,u+f0f6-f0f8,u+f10c,u+f114-f115,u+f118-f11a,u+f11c-f11d,u+f133,u+f147,u+f14e,u+f150-f152,u+f185-f186,u+f18e,u+f190-f192,u+f196,u+f1c1-f1c9,u+f1d9,u+f1db,u+f1e3,u+f1ea,u+f1f7,u+f1f9,u+f20a,u+f247-f248,u+f24a,u+f24d,u+f255-f25b,u+f25d,u+f271-f274,u+f278,u+f27b,u+f28c,u+f28e,u+f29c,u+f2b5,u+f2b7,u+f2ba,u+f2bc,u+f2be,u+f2c0-f2c1,u+f2c3,u+f2d0,u+f2d2,u+f2d4,u+f2dc}@font-face{font-family:"FontAwesome";font-display:block;src:url(https://ka-f.fontawesome.com/releases/v6.5.2/webfonts/free-fa-v4compatibility.woff2) format("woff2"),url(https://ka-f.fontawesome.com/releases/v6.5.2/webfonts/free-fa-v4compatibility.ttf) format("truetype");unicode-range:u+f041,u+f047,u+f065-f066,u+f07d-f07e,u+f080,u+f08b,u+f08e,u+f090,u+f09a,u+f0ac,u+f0ae,u+f0b2,u+f0d0,u+f0d6,u+f0e4,u+f0ec,u+f10a-f10b,u+f123,u+f13e,u+f148-f149,u+f14c,u+f156,u+f15e,u+f160-f161,u+f163,u+f175-f178,u+f195,u+f1f8,u+f219,u+f27a}</style><style media="all" id="fa-v5-font-face">/*!
+ * Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com
+ * License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
+ * Copyright 2024 Fonticons, Inc.
+ */@font-face{font-family:"Font Awesome 5 Brands";font-display:block;font-weight:400;src:url(https://ka-f.fontawesome.com/releases/v6.5.2/webfonts/free-fa-brands-400.woff2) format("woff2"),url(https://ka-f.fontawesome.com/releases/v6.5.2/webfonts/free-fa-brands-400.ttf) format("truetype")}@font-face{font-family:"Font Awesome 5 Free";font-display:block;font-weight:900;src:url(https://ka-f.fontawesome.com/releases/v6.5.2/webfonts/free-fa-solid-900.woff2) format("woff2"),url(https://ka-f.fontawesome.com/releases/v6.5.2/webfonts/free-fa-solid-900.ttf) format("truetype")}@font-face{font-family:"Font Awesome 5 Free";font-display:block;font-weight:400;src:url(https://ka-f.fontawesome.com/releases/v6.5.2/webfonts/free-fa-regular-400.woff2) format("woff2"),url(https://ka-f.fontawesome.com/releases/v6.5.2/webfonts/free-fa-regular-400.ttf) format("truetype")}</style><style media="all" id="fa-v4-shims">/*!
+ * Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com
+ * License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
+ * Copyright 2024 Fonticons, Inc.
+ </style>
+        <link href="all3/boxicons.min.css" rel="stylesheet">
+		<link href="all3/main.652df174.css" rel="stylesheet">
+</head>
+
+<body><noscript>Вам нужно включить JavaScript, чтобы пользоваться сайтом.</noscript>
+    <div id="root">
+        <nav class="NavBar_navBarScroll__qoIDu navbar navbar-expand-md fixed-top">
+            <div class="container-xl"><a class="navbar-brand mr-2" href="index.html">
+                    <div class="logo mr-2" style="background-image: url(https://i.imgur.com/cfeYKrs.png);"></div>
+                    Expensive<span>Beta</span>
+                </a><button aria-controls="navbar" type="button" aria-label="Toggle navigation" class="navbar-toggler collapsed"><span class="navbar-toggler-icon"></span></button>
+                <div class="navbar-collapse collapse" id="navbar">
+                        <hr class="mt-1 mb-1 d-md-none">
+                    </div>
+                    <div class="ml-auto navbar-nav"><a class="nav-link" href="products.php" target="_blank" rel="noreferrer">
+                        <i class="fa-solid fa-bag-shopping mr-2" aria-hidden="true"></i>Купить</a>
+                        <hr class="mt-1 mb-3 d-md-none"><a class="btn btn-lg btn-gradient" href="cabinet.php"><i class="fa-solid fa-user mr-2" aria-hidden="true"></i>Личный кабинет</a>
+                    </div>
+                </div>
+            </nav></div>
+        
+    <div class="Products_content__LIXMQ">
+        <div class="toast-container"></div>
+        <div class="Products_container__8gq5l container-xl">
+            <h3 class="mb-5">Наши услуги</h3>
+            <div class="Products_products__3dgHi">
+                <div class="Products_product__kEym8">
+<img src="https://i.imgur.com/cfeYKrs.png" alt="client image">
+                    <span>Клиент на 30 дней</span>
+                    <span>Цена: <b>150<i class="fa-solid fa-ruble-sign ml-1" aria-hidden="true"></i></b></span>
+                    <form action="https://funpay.com/lots/offer?id=32916299" method="POST" class="mt-3 w-100">
+                        <input type="hidden" name="product" value="30_days">
+                        <input type="hidden" name="amount" value="150">
+                        <button type="submit" class="btn btn-lg btn-gradient w-100">
+                            <i class="fa-solid fa-basket-shopping mr-2" aria-hidden="true"></i>Оплатить
+                        </button>
+                    </form>
+                </div>
+                <div class="Products_product__kEym8">
+<img src="https://i.imgur.com/cfeYKrs.png" alt="client image">
+                    <span>Клиент на 90 дней</span>
+                    <div class="d-flex">
+                        <span>Цена: <s class="text-danger"><b>450</b></s><b class="text-success">250<i class="fa-solid fa-ruble-sign ml-1" aria-hidden="true"></i></b></span>
+                        <div class="Products_discount__e6xD8">Скидка 10%</div>
+                    </div>
+                    <form action="https://funpay.com/lots/offer?id=32916489" method="POST" class="mt-3 w-100">
+                        <input type="hidden" name="product" value="180_days">
+                        <input type="hidden" name="amount" value="250">
+                        <button type="submit" class="btn btn-lg btn-gradient w-100">
+                            <i class="fa-solid fa-basket-shopping mr-2" aria-hidden="true"></i>Оплатить
+                        </button>
+                    </form>
+                </div>
+                <div class="Products_product__kEym8">
+<img src="https://i.imgur.com/cfeYKrs.png" alt="client image">
+                    <span>Клиент навсегда</span>
+                    <div class="d-flex">
+                        <span>Цена: <s class="text-danger"><b>600</b></s><b class="text-success">400<i class="fa-solid fa-ruble-sign ml-1" aria-hidden="true"></i></b></span>
+                        <div class="Products_discount__e6xD8">Скидка 25%</div>
+                    </div>
+                    <form action="https://funpay.com/lots/offer?id=32916161" method="POST" class="mt-3 w-100">
+                        <input type="hidden" name="product" value="forever">
+                        <input type="hidden" name="amount" value="400">
+                        <button type="submit" class="btn btn-lg btn-gradient w-100">
+                            <i class="fa-solid fa-basket-shopping mr-2" aria-hidden="true"></i>Оплатить
+                        </button>
+                    </form>
+                </div>
+                <div class="Products_product__kEym8">
+<img src="https://i.imgur.com/cfeYKrs.png" alt="client image">
+                    <span>Сброс HWID</span>
+                    <span>Цена: <b>100<i class="fa-solid fa-ruble-sign ml-1" aria-hidden="true"></i></b></span>
+                    <form action="https://funpay.com/lots/offer?id=34626692" method="POST" class="mt-3 w-100">
+                        <input type="hidden" name="product" value="hwid_reset">
+                        <input type="hidden" name="amount" value="100">
+                        <button type="submit" class="btn btn-lg btn-gradient w-100">
+                            <i class="fa-solid fa-basket-shopping mr-2" aria-hidden="true"></i>Оплатить
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <iframe height="1" width="1" style="position: absolute; top: 0px; left: 0px; border: none; visibility: hidden;" src="all3/saved_resource(1).html"></iframe>
+
+
+</body></html>
